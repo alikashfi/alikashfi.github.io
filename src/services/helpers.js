bdb1ddf395eb1ddd758a6cd8afb18dfc9adf0aa2
alikashfi <kashfi384@gmail.com>
2023-03-10 12:24:39 +0330

    build

--- src/services/helpers.js
+++
@@ -1,9 +0,0 @@
-const moment = require("jalali-moment");
-
-exports.formatDate = (date) => {
-    return moment(date).locale("fa").format("D MMM YYYY");
-};
-
-exports.stripTags = (text) => {
-    return text.replace(/<\/?[^>]+(>|$)/g, " ")
-}
